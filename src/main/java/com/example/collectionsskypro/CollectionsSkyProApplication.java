ea71d0527382dc35ef921d2c84d819f72b591d99
Kulachenkov <nik.k2710@gmail.com>
2022-01-26 09:59:01 +0300

    Initial commit

---
+++ src/main/java/com/example/collectionsskypro/CollectionsSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectionsskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsSkyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsSkyProApplication.class, args);
+    }
+
+}
